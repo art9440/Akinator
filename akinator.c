2e5336ea79f16bde09360d2cd7d8f5c5511c43fe
art9440 <art9440@gmail.com>
2023-12-17 23:18:55 +0700

    omg

--- akinator.c
+++ akinator.c
@@ -9,15 +9,18 @@ char * getSentence(char*buffer){
     int i = 0;
     int str_len = 0;
     char* sentence = (char*) malloc(sizeof (char));
+
     while (buffer[i] != '-')
         i++;
     i++;
+
     while (buffer[i] != '\0'){
         sentence[str_len] = buffer[i];
         str_len++;
         i++;
         sentence = (char*)realloc(sentence, 1000 * sizeof (char));
     }
+
     sentence[str_len] = '\0';
     return sentence;
 }
@@ -27,12 +30,14 @@ int getNum(char* buffer){
     int i = 0;
     int num_len = 0;
     char *num = (char*) malloc(sizeof (char));
+
     while (buffer[i] != '-'){
         num[num_len] = buffer[i];
         num_len++;
         i++;
         num = (char*)realloc(num ,1000 * sizeof (char));
     }
+
     num[num_len] = '\0';
     return atoi(num);
 
@@ -46,24 +51,41 @@ void FromFileToTree(TREE*tree, char*buffer){
 }
 
 
-void CreateNewData(char* savetxt, int savenum){
+void CreateNewData(char* savetxt, int savenum, FILE * file){
+    printf("%d", savenum);
     char correct_answ[256];
     char new_qstn[256];
     char new_answ[4];
-    int new_num;
-    char* buffer = (char*) malloc(sizeof (char));
-    char* save_answ;
+    int new_num1, new_num2;
+    new_num1 = savenum * 2;
+    new_num2 = savenum * 2 + 1;
+
     puts("What is the correct answer?");
     char c = getchar();
     gets( correct_answ);
     puts("With what question can your answer be distinguished from mine?");
     gets(new_qstn);
-    /*FILE * file;
-    file = fopen("DATAakinator.txt", "r+");
-    fread(buffer, )
-        fclose(file);*/
     puts("What is the answer for this question?");
     scanf("%s", new_answ);
+    file = fopen("DATAakinator.txt", "r+");
+    char string[256];
+    char **arr = NULL;
+    int arr_len = 0;
+
+    while (fgets(string, sizeof(string), file) != NULL){
+        arr = realloc(arr, (arr_len + 1) * sizeof(char*));
+        arr[arr_len] = strdup(string);
+        if (savenum == getNum(arr[arr_len])) {
+            char num = (char) savenum;
+            printf("%c*", num);
+        }
+        arr_len++;
+    }
+    fclose(file);
+
+    for (int i = 0;i<arr_len;i++){
+        printf("%s", arr[i]);
+    }
 }
 
 
@@ -78,6 +100,7 @@ int main(){
         FromFileToTree(tree, buffer);
     }
     fclose(file);
+
     while (tree != NULL){
         printf("%s", tree -> text);
         char answer[4];
@@ -94,7 +117,7 @@ int main(){
             int savenum = tree -> num;
             tree = tree->right;
             if (tree == NULL){
-                CreateNewData(savetxt, savenum);
+                CreateNewData(savetxt, savenum, file);
             }
         }
         else {
